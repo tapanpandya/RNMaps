fde91b887d5e25ba9cf21eb8c75268e22091573f
Tapan Pandya <tapanhpandya@gmail.com>
2021-02-12 00:33:58 -0500

    Setup Google Maps in react-native android module.

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <MapView
-          //  provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.map}
           region={{
             latitude: 37.78825,
