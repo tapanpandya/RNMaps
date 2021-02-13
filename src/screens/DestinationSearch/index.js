b8009c3e0f044397b54f8321c332fa8773c9e6ec
Tapan Pandya <tapanhpandya@gmail.com>
2021-02-12 23:03:33 -0500

    Implemented React native Google Autocomplete.

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,24 +1,49 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {TextInput, View} from 'react-native';
 import styles from './styles';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 
 const DestinationSearch = () => {
   const [fromText, setFromText] = useState('');
   const [destinationText, setDestinationText] = useState('');
 
+  const [originPlace, setOriginPlace] = useState(null);
+  const [destinationPlace, setDestinationPlace] = useState(null);
+
+  useEffect(() => {
+    if (originPlace && destinationPlace) {
+      console.warn('Redirect to results');
+    }
+  }, [originPlace, destinationPlace]);
+
   return (
     <View style={styles.container}>
-      <TextInput
-        value={fromText}
-        onChangeText={setFromText}
-        style={styles.textInput}
-        placeholder="From"
+      <GooglePlacesAutocomplete
+        styles={{textInput: styles.textInput}}
+        placeholder="From?"
+        onPress={(data, details = null) => {
+          setOriginPlace({data, details});
+          console.log(data, details);
+        }}
+        fetchDetails
+        query={{
+          key: 'AIzaSyDcPI1_TdYdbzbYjuwjckJ6TKgQvWVXn8E',
+          language: 'en',
+        }}
       />
-      <TextInput
-        value={destinationText}
-        onChangeText={setDestinationText}
-        style={styles.textInput}
+
+      <GooglePlacesAutocomplete
+        styles={{textInput: styles.textInput}}
         placeholder="Where to?"
+        onPress={(data, details = null) => {
+          setDestinationPlace({data, details});
+          console.log(data, details);
+        }}
+        fetchDetails
+        query={{
+          key: 'AIzaSyDcPI1_TdYdbzbYjuwjckJ6TKgQvWVXn8E',
+          language: 'en',
+        }}
       />
     </View>
   );
